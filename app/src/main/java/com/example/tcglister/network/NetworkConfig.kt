29ca97d1d6c6700ac4a4fd328f72f5fe04092ddd
Finta Norbert <norbi.f95@gmail.com>
2021-04-11 14:41:43 +0200

    Update api endpoint

--- app/src/main/java/com/example/tcglister/network/NetworkConfig.kt
+++ app/src/main/java/com/example/tcglister/network/NetworkConfig.kt
@@ -1,6 +1,6 @@
 package com.example.tcglister.network
 
 object NetworkConfig {
-    const val API_ENDPOINT_ADDRESS = ""
-    const val TOKEN_ENDPOINT_ADDRESS = ""
+    const val API_ENDPOINT_ADDRESS = "https://api.pokemontcg.io/v2/"
+    const val TOKEN_ENDPOINT_ADDRESS = "https://api.pokemontcg.io/v2/"
 }
